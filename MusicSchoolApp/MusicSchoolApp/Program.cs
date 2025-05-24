6a421e94553b96112203bbe04d2a97f9cb0a0bf3
SP6608 <spapanov@gmail.com>
2025-05-24 14:32:22 +0300

    Задача 28

--- MusicSchoolApp/MusicSchoolApp/Program.cs
+++ MusicSchoolApp/MusicSchoolApp/Program.cs
@@ -4,19 +4,65 @@
     {
         static void Main(string[] args)
         {
-			try
-			{
-                string[] input = File.ReadAllLines("input.txt");
-                foreach (string line in input) 
-                {
-                    Console.WriteLine(line);
-                }
-			}
-			catch (Exception )
-			{
 
-                Console.WriteLine("Specialty cannot be an empty string!");
-			}
+
+            string[] input = File.ReadAllLines("input.txt");
+            List<SchoolMember> members = new List<SchoolMember>();
+            int br = 0;
+            foreach (string line in input)
+            {
+                br++;
+                try
+                {
+                    var data = line.Split(";").ToList();
+                    string type = data[0];
+                    string name = data[1];
+                    int age = int.Parse(data[2]);
+                    if (type == "Student")
+                    {
+                        string Instrument = data[3];
+                        int practiceHours = int.Parse(data[4]);
+                        SchoolMember student = new Student(name, age, Instrument, practiceHours);
+                        members.Add(student);
+                    }
+                    else if (type == "Teacher")
+                    {
+                        try
+                        {
+
+                            var result = Enum.TryParse(data[3], out Speciality speciality);
+                            int studentsCount = int.Parse(data[4]);
+                            double salary = double.Parse(data[5]);
+                            SchoolMember member = new Teacher(name, age, speciality, studentsCount, salary);
+                            members.Add(member);
+                        }
+                        catch (Exception)
+                        {
+
+                            Console.WriteLine("Specialty cannot be an empty string!");
+                        }
+
+                    }
+                    else if (type == "Manager")
+                    {
+                        double budget = double.Parse(data[3]);
+                        int yearsInService = int.Parse(data[4]);
+                        SchoolMember member = new Manager(name, age, budget, yearsInService);
+                        members.Add(member);
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine($"Invalid data on line {br}: {ex.Message}");
+                }
+            }//end foreach
+            foreach (var member in members)
+            {
+
+            }
+
+
         }
     }
 }
--- MusicSchoolApp/MusicSchoolApp/obj/Debug/net8.0/MusicSchoolApp.AssemblyInfo.cs
+++ MusicSchoolApp/MusicSchoolApp/obj/Debug/net8.0/MusicSchoolApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicSchoolApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21054f6a1871816a0c37c7b620dc8c9c779b4da5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7a1327dd97ab46c32513c6112b6f85f19300b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicSchoolApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicSchoolApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
