22347e27b13be42c3e2780ebd35c9defe9e9f5c4
SP6608 <spapanov@gmail.com>
2025-06-09 16:16:24 +0300

    Задачи от SoftUni

--- MusicSchoolApp/MusicSchoolApp/obj/Debug/net8.0/MusicSchoolApp.AssemblyInfo.cs
+++ MusicSchoolApp/MusicSchoolApp/obj/Debug/net8.0/MusicSchoolApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicSchoolApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02dec4504a728e9a3e3a83a96eb54d9258f199d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5165051ebf558a5011d8766f05441003e30fb151")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicSchoolApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicSchoolApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
